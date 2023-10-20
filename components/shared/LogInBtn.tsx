60f987268b38ab005d60d848c50dff59108b9395
Alexey Osychenko <alexeyosychencko@gmail.com>
2023-10-20 16:23:10 +0300

    fix login/signin mb

--- components/shared/LogInBtn.tsx
+++ components/shared/LogInBtn.tsx
@@ -1,13 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 export const LogInBtn = (): React.ReactElement => {
   return (
-    <Link
-      href="/sign-in"
-      className="btn-secondary small-medium min-h-[41px] w-full rounded-lg px-4 py-3 text-center"
-    >
-      <span className="primary-text-gradient">Log In</span>
+    <Link href="/sign-in">
+      <Button className="btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3">
+        <Image
+          src="/assets/icons/account.svg"
+          alt="sign up"
+          width={20}
+          height={20}
+          className="invert-colors lg:hidden"
+        />
+        <span className="primary-text-gradient max-lg:hidden">Log In</span>
+      </Button>
     </Link>
   );
 };
--- components/shared/SignInBtn.tsx
+++ components/shared/SignInBtn.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -5,9 +6,16 @@ export const SignInBtn = (): React.ReactElement => {
   return (
     <Link
       href="/sign-up"
-      className="btn-tertiary  small-medium min-h-[41px] w-full rounded-lg px-4 py-3 text-center"
+      className="small-medium light-border-2 btn-tertiary text-dark400_light900 flex min-h-[41px] w-full justify-center rounded-lg border px-4 py-3 shadow-none"
     >
-      Sign Up
+      <Image
+        src="/assets/icons/sign-up.svg"
+        alt="login"
+        width={20}
+        height={20}
+        className="invert-colors lg:hidden"
+      />
+      <span className="max-lg:hidden">Sign Up</span>
     </Link>
   );
 };
