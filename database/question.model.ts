04264b2cd69c198d1cf3e7804baf6df7f63b3d91
Alexey Osychenko <alexeyosychencko@gmail.com>
2023-10-28 14:03:55 +0300

    db model: Question

---
+++ database/question.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model, Document, models } from "mongoose";
+
+interface Question extends Document {
+  title: string;
+  explanation: string;
+  tags: Schema.Types.ObjectId[];
+  views: number;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId;
+  answers: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const questionSchema = new Schema<Question>({
+  title: { type: String, required: true },
+  explanation: { type: String, required: true },
+  tags: { type: [Schema.Types.ObjectId], ref: "Tag" },
+  views: { type: Number, default: 0 },
+  upvotes: { type: [Schema.Types.ObjectId], ref: "User", default: [] },
+  downvotes: { type: [Schema.Types.ObjectId], ref: "User", default: [] },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  answers: { type: [Schema.Types.ObjectId], ref: "Answer" },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const QuestionModel =
+  models.Question || model<Question>("Question", questionSchema);
+
+export default QuestionModel;
