41147e742c4199618967e1fa12c34504e89c559c
Alexey Osychenko <alexeyosychencko@gmail.com>
2023-11-28 19:12:00 +0200

    names

--- components/right-sidebar/RightSidebar.tsx
+++ components/right-sidebar/RightSidebar.tsx
@@ -12,7 +12,7 @@ const RightSidebar = async (): Promise<ReactElement> => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[330px] flex-col overflow-y-auto border-l p-6 pt-28 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
-        <h3 className="h3-bold text-dark200_light900">Hot Networks</h3>
+        <h3 className="h3-bold text-dark200_light900">Hot networks</h3>
         <ul className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <li key={question.id}>
@@ -36,7 +36,7 @@ const RightSidebar = async (): Promise<ReactElement> => {
         </ul>
       </div>
       <div className="mt-16">
-        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <h3 className="h3-bold text-dark200_light900">Popular tags</h3>
         <ul className="mt-7 flex w-full flex-col gap-4">
           {popularTags.map((tag) => (
             <li key={tag._id}>
